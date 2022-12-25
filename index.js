7a3164a763db2150ee324e5a878db6f2c83d867e
Mahinkahn <mahinkahn37@gmail.com>
2022-12-25 13:27:44 +0600

    ams

--- index.js
+++ index.js
@@ -13,19 +13,36 @@ app.use(express.json());
 
 
 
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jnh0xtx.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run() {
     try {
         const appointmentOptionCollection = client.db('doctorsPortal').collection('appointmentOptions');
+        const bookingsCollection = client.db('doctorsPortal').collection('bookings');
 
         app.get('/appointmentOptions', async (req, res) => {
             const query = {};
             const options = await appointmentOptionCollection.find(query).toArray()
             res.send(options);
+        });
+
+        /***
+         * API Naming Convention
+         * app.get('/bookings')
+         * app.get('/bookings/:id')
+         * app.post('/bookings')
+         * app.patch('/bookings/:id')
+         * app.delete('/bookings/:id')
+         */
+
+        app.post('/bookings', async (req, res) => {
+            const booking = req.body
+            console.log(booking);
+            const result = await bookingsCollection.insertOne(booking);
+            res.send(result);
         })
+
     }
     finally {
 
