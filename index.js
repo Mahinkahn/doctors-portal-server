d075fef27daa33c862ccf968cfc2ce09b4a4a3ff
Mahinkahn <mahinkahn37@gmail.com>
2022-12-26 13:19:11 +0600

    on

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ async function run() {
     try {
         const appointmentOptionCollection = client.db('doctorsPortal').collection('appointmentOptions');
         const bookingsCollection = client.db('doctorsPortal').collection('bookings');
+        const usersCollection = client.db('doctorsPortal').collection('users');
 
         app.get('/appointmentOptions', async (req, res) => {
             const date = req.query.date;
@@ -47,6 +48,13 @@ async function run() {
          * app.delete('/bookings/:id')
          */
 
+        app.get('/bookings', async (req, res) => {
+            const email = req.query.email;
+            const query = { email: email };
+            const bookings = await bookingsCollection.find(query).toArray();
+            res.send(bookings);
+        })
+
         app.post('/bookings', async (req, res) => {
             const booking = req.body;
             console.log(booking);
@@ -62,6 +70,12 @@ async function run() {
             }
 
             const result = await bookingsCollection.insertOne(booking);
+            res.send(result);
+        });
+
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const result = await usersCollection.insertOne(user);
             res.send(result);
         })
 
